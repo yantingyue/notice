f54c35c1e06c3651c0a8be08cc0690e877aa6ed2
yantingyue <784293715@qq.com>
2023-07-29 21:51:12 +0800

    url

--- app/job/feishu_notice/feishu_url.go
+++ app/job/feishu_notice/feishu_url.go
@@ -71,14 +71,14 @@ func FeiShuUrl(text string, token string) {
 	if _, ok := tokenUrlMap[token]; ok {
 		for k, v := range tokenUrlMap[token] {
 			if k > 0 && k < 2 {
-				time.Sleep(time.Second * 3)
+				time.Sleep(time.Second * 2)
 			}
-			go func() {
-				_, err := cli.Post(v, nil, jsonBytes)
-				if err != nil {
-					log.Println(err)
-				}
-			}()
+			//go func() {
+			_, err := cli.Post(v, nil, jsonBytes)
+			if err != nil {
+				log.Println(err)
+			}
+			//}()
 
 		}
 	}
