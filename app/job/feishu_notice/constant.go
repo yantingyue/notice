ec6ea859b7325fcecfdaba79bb0c5dc44e0fc67e
zhoumuhao <1304718335@qq.com>
2023-08-05 11:35:41 +0800

    feat

--- app/job/feishu_notice/constant.go
+++ app/job/feishu_notice/constant.go
@@ -5,8 +5,8 @@ import "time"
 const (
 	Token1 = "21e23fa4467649b2bce29687cc037aba" //case
 	Token2 = "16bfdee054d9476fa5a1816a9e13ba0e" //富豪
-	Token3 = "70147bbcd9704db2a9b48b25cc7fdca6" //145
-	Token4 = "104c4c5a82e546bca8b64f8d3c056345" //谜语人
+	Token3 = "d83e6d0325e44e8f846171892794d4b5" //145
+	Token4 = "6f734229b34b40f286bfa6e9ccd66422" //谜语人
 	Token5 = "edf1c55bd0d54a92a02dfb7ed8870b84" //柴郡猫
 	Token6 = "6d442761435a43169ca3c8ada2fc7b59" //配置
 	Token7 = "92cd4037b3d347869308f027e59f2551" //陆逊
