90315662ba2e91e7dbc4b7a91ab402713cf7e19b
yantingyue <784293715@qq.com>
2023-08-09 16:32:54 +0800

    notice

--- app/job/feishu_notice/buy_notice.go
+++ app/job/feishu_notice/buy_notice.go
@@ -37,7 +37,7 @@ func handle(ctx context.Context, token string, name string) {
 		resultMap := make(map[uint64]struct{})
 		for _, v := range res.Data.Result {
 			resultMap[v.ProductId] = struct{}{}
-			if v.ProductId == 1019723 || v.ProductId == 1019287 || v.ProductId == 1019119 {
+			if v.ProductId == 1019723 || v.ProductId == 1019287 || v.ProductId == 1019119 || v.ProductId == 1019555 {
 				continue
 			}
 			noticeKey := fmt.Sprintf("%s:%d", name, v.ProductId)
