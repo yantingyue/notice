a3f0b47e12bc5d9c68cff4d471f045e1a475a33c
yantingyue <784293715@qq.com>
2023-08-13 14:57:56 +0800

    notice

--- app/job/feishu_notice/constant.go
+++ app/job/feishu_notice/constant.go
@@ -13,7 +13,7 @@ const (
 	Token8  = "0c4090fcf6c44c46a2fdcddfd39f6cb9" //145(胡莹)
 	Token9  = "daaf58be3fb64cba842ec858d81d5589" //门童
 	Token10 = "891e817b1ab24c9c89c81efef2921888" //富豪小号
-	Token11 = "172b93535df640c1a0033da80a96d549" //蜥蜴绝缘体
+	Token11 = "fe274107d9d046808a89beeb5b75e325" //蜥蜴绝缘体
 )
 
 const (
