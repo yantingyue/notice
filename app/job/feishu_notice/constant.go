0c99f07f7dc8cf66a51778a23af33c8d2805fa0e
yantingyue <784293715@qq.com>
2023-08-14 20:26:04 +0800

    hd

--- app/job/feishu_notice/constant.go
+++ app/job/feishu_notice/constant.go
@@ -13,7 +13,7 @@ const (
 	Token8  = "0c4090fcf6c44c46a2fdcddfd39f6cb9" //145(胡莹)
 	Token9  = "daaf58be3fb64cba842ec858d81d5589" //门童
 	Token10 = "891e817b1ab24c9c89c81efef2921888" //富豪小号
-	Token11 = "fe274107d9d046808a89beeb5b75e325" //蜥蜴绝缘体
+	Token11 = "97b7776c732f4940b781d7eba2c3355a" //蜥蜴绝缘体
 )
 
 const (
