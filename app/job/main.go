4053aa3281e308fc131c289d9002fc8cb8184191
zhoumuhao <1304718335@qq.com>
2023-08-22 10:36:11 +0800

    feat

--- app/job/main.go
+++ app/job/main.go
@@ -60,21 +60,21 @@ func cronjob() {
 	}); err != nil {
 		panic(err)
 	}
-	if err := c.AddFunc("*/5 * * * * *", func() {
-		feishu_notice.MotorNotice("富豪小号", feishu_notice.UserId9)
-	}); err != nil {
-		panic(err)
-	}
+	//if err := c.AddFunc("*/5 * * * * *", func() {
+	//	feishu_notice.MotorNotice("富豪小号", feishu_notice.UserId9)
+	//}); err != nil {
+	//	panic(err)
+	//}
 	if err := c.AddFunc("*/7 * * * * *", func() {
 		feishu_notice.MotorNotice("蜥蜴绝缘体", feishu_notice.UserId10)
 	}); err != nil {
 		panic(err)
 	}
-	if err := c.AddFunc("*/7 * * * * *", func() {
-		feishu_notice.MotorNotice("法典人", feishu_notice.UserId11)
-	}); err != nil {
-		panic(err)
-	}
+	//if err := c.AddFunc("*/7 * * * * *", func() {
+	//	feishu_notice.MotorNotice("法典人", feishu_notice.UserId11)
+	//}); err != nil {
+	//	panic(err)
+	//}
 	if err := c.AddFunc("*/5 * * * * *", func() {
 		feishu_notice.MotorNotice("145韩新枝", feishu_notice.UserId13)
 	}); err != nil {
