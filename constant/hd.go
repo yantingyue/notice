612af0327b54b78ea867f56f7a2d1776e01dbf37
yantingyue <784293715@qq.com>
2023-08-27 09:46:12 +0800

    no

--- constant/hd.go
+++ constant/hd.go
@@ -1,27 +1,6 @@
 package constant
 
 const (
-	MysqlHost   = ""
-	MysqlPort   = ""
-	MysqlUser   = ""
-	MysqlPasswd = ""
-	MysqlDB     = ""
-)
-
-const (
-	RedisHost = ""
-	RedisPort = ""
-)
-
-const (
-	ApiHost            = "https://api.aichaoliuapp.cn"
-	SecondSellListPath = "/aiera/ai_match_trading/nft_second/sell_product/list"
-	CreateOrderPath    = "/aiera/ai_match_trading/nft_second/sell_order/pay"
-	SearchProductPath  = "/aiera/v2/hotdog/search/product/search"
-	WalletPayPath      = "/aiera/ai_match_trading/nft_second/sell_order/wallet_order"
-)
-
-const (
 	UserId   = ""
 	Token    = "fb30c500ff2346cb86fb27cd8935bf15"
 	Salt     = "5c33494d1b277902d1b78f98093f6fd4"
