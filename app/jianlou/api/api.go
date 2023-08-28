4e73b74356572d43340ad3b3915468d0da22a3df
yantingyue <784293715@qq.com>
2023-08-28 13:25:04 +0800

    nd

--- app/jianlou/api/api.go
+++ app/jianlou/api/api.go
@@ -56,7 +56,6 @@ func Grab(ctx context.Context, token string, body map[string]interface{}) {
 			if _, ok := SecondIdMap[sellInfo.SecondId]; ok {
 				if len(SecondIdMap) >= 20 {
 					SecondIdMap = make(map[uint64]struct{})
-					SecondIdMap = nil
 				}
 				continue
 			}
