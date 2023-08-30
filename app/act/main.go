660c40f40a93f490dc7fc43e765b3784af7e2f35
yantingyue <784293715@qq.com>
2023-08-30 22:16:45 +0800

    cs

--- app/act/main.go
+++ app/act/main.go
@@ -1,277 +1,278 @@
 package main
 
-import (
-	"context"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"github.com/pkg/errors"
-	"github.com/spf13/cast"
-	"github.com/valyala/fasthttp"
-	"log"
-	"notice/internal/cli"
-	"sync"
-	"time"
-)
-
-type ReplaceResp struct {
-	Code int32  `json:"code"`
-	Msg  string `json:"msg"`
-	Data struct {
-		OnSaleStatus       uint32 `json:"on_sale_status"`
-		CurrentMilliTime   int64  `json:"current_milli_time"`
-		StartTimeTimestamp int64  `json:"start_time_timestamp"`
-		EndTimestamp       int64  `json:"end_time_timestamp"`
-	} `json:"data"`
-}
-type ReplaceTimeResp struct {
-	Code             int32  `json:"code"`
-	Msg              string `json:"msg"`
-	CurrentMilliTime int64  `json:"current_milli_time"`
-}
-
-type ResponseData struct {
-	Code int    `json:"code"`
-	Msg  string `json:"msg"`
-	Data []struct {
-		OrderID    uint64 `json:"order_id"`
-		Title      string `json:"title"`
-		Picture    string `json:"picture"`
-		UpdateTime string `json:"update_time"`
-		SubTitle   string `json:"sub_title"`
-		Type       string `json:"type"`
-		Weight     int    `json:"weight"`
-	} `json:"data"`
-}
-
-var (
-	id uint64
-)
-
-func init() {
-	cli.InitRedisClient()
-}
-func main() {
-	ctx := context.Background()
-	i, _ := cli.RedisClient.Get(ctx, "act").Result()
-	if i != "" {
-		id = cast.ToUint64(i)
-		for {
-			if b == 1 {
-				Fj()
-			}
-			if b == 2 {
-				Rp()
-			}
-		}
-	}
-}
-
-const (
-	b                 = 2                                  //1是分解 2是置换
-	actId             = 535                                //活动id
-	tokenYanTingYue   = "3662106dd9b749d3995348c5f2884a5b" //颜庭跃
-	tokenYanTingYueDa = "24715fa709414f6eb364ffb6f8c13485" //颜庭跃
-)
-
-func Fj() {
-	orderInfo := GetOrderId(id, tokenYanTingYue)
-	if orderInfo.Code == 0 && orderInfo.Msg == "success" && len(orderInfo.Data) > 0 {
-		for {
-			if FjDetail(id, tokenYanTingYue) {
-				break
-			}
-		}
-		for _, v := range orderInfo.Data {
-			go Replace(id, v.OrderID, tokenYanTingYue)
-			time.Sleep(time.Millisecond * 2000)
-		}
-	}
-	time.Sleep(time.Millisecond * 800)
-}
-
-func Rp() {
-	orderInfo := GetOrderId(id, tokenYanTingYue)
-	if orderInfo.Code == 0 && orderInfo.Msg == "success" && len(orderInfo.Data) > 0 {
-		for {
-			if ReplaceDetail(id, tokenYanTingYue) {
-				break
-			}
-		}
-		for _, v := range orderInfo.Data {
-			go Replace(id, v.OrderID, tokenYanTingYue)
-			time.Sleep(time.Millisecond * 2000)
-		}
-	}
-	time.Sleep(time.Millisecond * 2000)
-}
-
-func GetOrderId(id uint64, token string) (res ResponseData) {
-	header := GenerateHeader1(token)
-	body := map[string]interface{}{
-		"replace_id": id,
-		"pageNumber": 1,
-		"pageSize":   5,
-	}
-	jsonBytes, _ := json.Marshal(body)
-	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/combination/choice/material", header, jsonBytes)
-	log.Println(string(resp))
-	json.Unmarshal(resp, &res)
-	return res
-}
-
-func FjDetail(id uint64, token string) bool {
-	header := GenerateHeader1(token)
-	body := map[string]interface{}{
-		"replace_id": id,
-	}
-	jsonBytes, _ := json.Marshal(body)
-	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/replace/active/detail", header, jsonBytes)
-	log.Println(string(resp))
-	if len(resp) == 0 {
-		return false
-	}
-	res := ReplaceResp{}
-	json.Unmarshal(resp, &res)
-	if res.Code == 0 && res.Msg == "success" && res.Data.OnSaleStatus == 1 {
-		return true
-	}
-	return false
-}
-func ReplaceDetail(id uint64, token string) bool {
-	header := GenerateHeader1(token)
-	body := map[string]interface{}{
-		"replace_id": id,
-	}
-	jsonBytes, _ := json.Marshal(body)
-	var (
-		wg        sync.WaitGroup
-		resDetail = ReplaceResp{}
-		resTime   = ReplaceTimeResp{}
-		resp1     []byte
-		resp2     = make(map[string]interface{})
-	)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		resp1, _ = Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/replace/active/detail", header, jsonBytes)
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		resp2, _ = Get("https://api.aichaoliuapp.cn/aiera/current/milli/time")
-	}()
-	wg.Wait()
-	if len(resp1) == 0 || len(resp2) == 0 {
-		return false
-	}
-	json.Unmarshal(resp1, &resDetail)
-	g, _ := json.Marshal(resp2)
-	json.Unmarshal(g, &resTime)
-	//resDetail.Data.StartTimeTimestamp = 1689605160000
-	diffTime := resDetail.Data.StartTimeTimestamp - resTime.CurrentMilliTime
-	log.Println(resDetail.Data.StartTimeTimestamp, resTime.CurrentMilliTime, diffTime)
-	if diffTime < 100 && diffTime > 0 {
-		//time.Sleep(time.Millisecond * time.Duration(diffTime))
-		log.Println(diffTime, resTime.CurrentMilliTime, resDetail.Data.StartTimeTimestamp, time.Now().UnixMilli())
-		return true
-	}
-	if resDetail.Code == 0 && resDetail.Msg == "success" && resTime.CurrentMilliTime >= resDetail.Data.StartTimeTimestamp && resTime.CurrentMilliTime <= resDetail.Data.EndTimestamp {
-		return true
-	}
-	return false
-}
-func Replace(id, orderId uint64, token string) bool {
-	header := GenerateHeader1(token)
-	body := map[string]interface{}{
-		"order_id":   orderId,
-		"replace_id": id,
-	}
-	jsonBytes, _ := json.Marshal(body)
-	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/replace/active/exchange", header, jsonBytes)
-
-	log.Println(orderId, string(resp))
-	if len(resp) == 0 {
-		return false
-	}
-	res := ReplaceResp{}
-	json.Unmarshal(resp, &res)
-	if res.Code == 0 && res.Msg == "success" {
-		return true
-	}
-	return false
-}
-
-const (
-	timeOut  = 60 * time.Second
-	version  = "31850"
-	channel  = "010100"
-	platform = "ios"
-	appname  = "aiera.sneaker.snkrs.shoe"
-	salt     = "5c33494d1b277902d1b78f98093f6fd4"
-)
-
-func GenerateHeader1(token string) map[string]string {
-	timestamp := cast.ToString(time.Now().UnixMilli())
-	return map[string]string{
-		"token":     token,
-		"version":   version,
-		"channel":   channel,
-		"platform":  platform,
-		"appname":   appname,
-		"timestamp": timestamp,
-		"sign":      MD5(timestamp + salt),
-	}
-}
-
-func Get(host string) (m map[string]interface{}, err error) {
-	statusCode, body, err := fasthttp.GetTimeout(nil, host, timeOut)
-	if err != nil {
-		return
-	}
-	if statusCode != fasthttp.StatusOK {
-		err = errors.New(fmt.Sprintf("request failed statusCode[%d]", statusCode))
-		return
-	}
-	if body == nil {
-		err = errors.New("response body is nil")
-		return
-	}
-	m = make(map[string]interface{})
-	if err = json.Unmarshal(body, &m); err != nil {
-		return
-	}
-	return
-}
-
-func Post(host string, header map[string]string, payload []byte) (body []byte, err error) {
-	req := &fasthttp.Request{}
-	req.SetRequestURI(host)
-
-	req.Header.SetContentType("application/json")
-	req.Header.SetMethod("POST")
-
-	if header != nil {
-		for k, v := range header {
-			req.Header.Set(k, v)
-		}
-	}
-
-	req.SetBody(payload)
-
-	resp := &fasthttp.Response{}
-	client := &fasthttp.Client{}
-	if err = client.DoTimeout(req, resp, timeOut); err != nil {
-		return
-	}
-	body = resp.Body()
-	return
-}
-
-// 生成32位MD5
-func MD5(text string) string {
-	ctx := md5.New()
-	ctx.Write([]byte(text))
-	return hex.EncodeToString(ctx.Sum(nil))
-}
+//
+//import (
+//	"context"
+//	"crypto/md5"
+//	"encoding/hex"
+//	"encoding/json"
+//	"fmt"
+//	"github.com/pkg/errors"
+//	"github.com/spf13/cast"
+//	"github.com/valyala/fasthttp"
+//	"log"
+//	"notice/internal/cli"
+//	"sync"
+//	"time"
+//)
+//
+//type ReplaceResp struct {
+//	Code int32  `json:"code"`
+//	Msg  string `json:"msg"`
+//	Data struct {
+//		OnSaleStatus       uint32 `json:"on_sale_status"`
+//		CurrentMilliTime   int64  `json:"current_milli_time"`
+//		StartTimeTimestamp int64  `json:"start_time_timestamp"`
+//		EndTimestamp       int64  `json:"end_time_timestamp"`
+//	} `json:"data"`
+//}
+//type ReplaceTimeResp struct {
+//	Code             int32  `json:"code"`
+//	Msg              string `json:"msg"`
+//	CurrentMilliTime int64  `json:"current_milli_time"`
+//}
+//
+//type ResponseData struct {
+//	Code int    `json:"code"`
+//	Msg  string `json:"msg"`
+//	Data []struct {
+//		OrderID    uint64 `json:"order_id"`
+//		Title      string `json:"title"`
+//		Picture    string `json:"picture"`
+//		UpdateTime string `json:"update_time"`
+//		SubTitle   string `json:"sub_title"`
+//		Type       string `json:"type"`
+//		Weight     int    `json:"weight"`
+//	} `json:"data"`
+//}
+//
+//var (
+//	id uint64
+//)
+//
+//func init() {
+//	cli.InitRedisClient()
+//}
+//func main() {
+//	ctx := context.Background()
+//	i, _ := cli.RedisClient.Get(ctx, "act").Result()
+//	if i != "" {
+//		id = cast.ToUint64(i)
+//		for {
+//			if b == 1 {
+//				Fj()
+//			}
+//			if b == 2 {
+//				Rp()
+//			}
+//		}
+//	}
+//}
+//
+//const (
+//	b                 = 2                                  //1是分解 2是置换
+//	actId             = 535                                //活动id
+//	tokenYanTingYue   = "3662106dd9b749d3995348c5f2884a5b" //颜庭跃
+//	tokenYanTingYueDa = "24715fa709414f6eb364ffb6f8c13485" //颜庭跃
+//)
+//
+//func Fj() {
+//	orderInfo := GetOrderId(id, tokenYanTingYue)
+//	if orderInfo.Code == 0 && orderInfo.Msg == "success" && len(orderInfo.Data) > 0 {
+//		for {
+//			if FjDetail(id, tokenYanTingYue) {
+//				break
+//			}
+//		}
+//		for _, v := range orderInfo.Data {
+//			go Replace(id, v.OrderID, tokenYanTingYue)
+//			time.Sleep(time.Millisecond * 2000)
+//		}
+//	}
+//	time.Sleep(time.Millisecond * 800)
+//}
+//
+//func Rp() {
+//	orderInfo := GetOrderId(id, tokenYanTingYue)
+//	if orderInfo.Code == 0 && orderInfo.Msg == "success" && len(orderInfo.Data) > 0 {
+//		for {
+//			if ReplaceDetail(id, tokenYanTingYue) {
+//				break
+//			}
+//		}
+//		for _, v := range orderInfo.Data {
+//			go Replace(id, v.OrderID, tokenYanTingYue)
+//			time.Sleep(time.Millisecond * 2000)
+//		}
+//	}
+//	time.Sleep(time.Millisecond * 2000)
+//}
+//
+//func GetOrderId(id uint64, token string) (res ResponseData) {
+//	header := GenerateHeader1(token)
+//	body := map[string]interface{}{
+//		"replace_id": id,
+//		"pageNumber": 1,
+//		"pageSize":   5,
+//	}
+//	jsonBytes, _ := json.Marshal(body)
+//	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/combination/choice/material", header, jsonBytes)
+//	log.Println(string(resp))
+//	json.Unmarshal(resp, &res)
+//	return res
+//}
+//
+//func FjDetail(id uint64, token string) bool {
+//	header := GenerateHeader1(token)
+//	body := map[string]interface{}{
+//		"replace_id": id,
+//	}
+//	jsonBytes, _ := json.Marshal(body)
+//	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/replace/active/detail", header, jsonBytes)
+//	log.Println(string(resp))
+//	if len(resp) == 0 {
+//		return false
+//	}
+//	res := ReplaceResp{}
+//	json.Unmarshal(resp, &res)
+//	if res.Code == 0 && res.Msg == "success" && res.Data.OnSaleStatus == 1 {
+//		return true
+//	}
+//	return false
+//}
+//func ReplaceDetail(id uint64, token string) bool {
+//	header := GenerateHeader1(token)
+//	body := map[string]interface{}{
+//		"replace_id": id,
+//	}
+//	jsonBytes, _ := json.Marshal(body)
+//	var (
+//		wg        sync.WaitGroup
+//		resDetail = ReplaceResp{}
+//		resTime   = ReplaceTimeResp{}
+//		resp1     []byte
+//		resp2     = make(map[string]interface{})
+//	)
+//	wg.Add(1)
+//	go func() {
+//		defer wg.Done()
+//		resp1, _ = Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/replace/active/detail", header, jsonBytes)
+//	}()
+//	wg.Add(1)
+//	go func() {
+//		defer wg.Done()
+//		resp2, _ = Get("https://api.aichaoliuapp.cn/aiera/current/milli/time")
+//	}()
+//	wg.Wait()
+//	if len(resp1) == 0 || len(resp2) == 0 {
+//		return false
+//	}
+//	json.Unmarshal(resp1, &resDetail)
+//	g, _ := json.Marshal(resp2)
+//	json.Unmarshal(g, &resTime)
+//	//resDetail.Data.StartTimeTimestamp = 1689605160000
+//	diffTime := resDetail.Data.StartTimeTimestamp - resTime.CurrentMilliTime
+//	log.Println(resDetail.Data.StartTimeTimestamp, resTime.CurrentMilliTime, diffTime)
+//	if diffTime < 100 && diffTime > 0 {
+//		//time.Sleep(time.Millisecond * time.Duration(diffTime))
+//		log.Println(diffTime, resTime.CurrentMilliTime, resDetail.Data.StartTimeTimestamp, time.Now().UnixMilli())
+//		return true
+//	}
+//	if resDetail.Code == 0 && resDetail.Msg == "success" && resTime.CurrentMilliTime >= resDetail.Data.StartTimeTimestamp && resTime.CurrentMilliTime <= resDetail.Data.EndTimestamp {
+//		return true
+//	}
+//	return false
+//}
+//func Replace(id, orderId uint64, token string) bool {
+//	header := GenerateHeader1(token)
+//	body := map[string]interface{}{
+//		"order_id":   orderId,
+//		"replace_id": id,
+//	}
+//	jsonBytes, _ := json.Marshal(body)
+//	resp, _ := Post("https://api.aichaoliuapp.cn/aiera/ai_match_trading/nft/replace/active/exchange", header, jsonBytes)
+//
+//	log.Println(orderId, string(resp))
+//	if len(resp) == 0 {
+//		return false
+//	}
+//	res := ReplaceResp{}
+//	json.Unmarshal(resp, &res)
+//	if res.Code == 0 && res.Msg == "success" {
+//		return true
+//	}
+//	return false
+//}
+//
+//const (
+//	timeOut  = 60 * time.Second
+//	version  = "31850"
+//	channel  = "010100"
+//	platform = "ios"
+//	appname  = "aiera.sneaker.snkrs.shoe"
+//	salt     = "5c33494d1b277902d1b78f98093f6fd4"
+//)
+//
+//func GenerateHeader1(token string) map[string]string {
+//	timestamp := cast.ToString(time.Now().UnixMilli())
+//	return map[string]string{
+//		"token":     token,
+//		"version":   version,
+//		"channel":   channel,
+//		"platform":  platform,
+//		"appname":   appname,
+//		"timestamp": timestamp,
+//		"sign":      MD5(timestamp + salt),
+//	}
+//}
+//
+//func Get(host string) (m map[string]interface{}, err error) {
+//	statusCode, body, err := fasthttp.GetTimeout(nil, host, timeOut)
+//	if err != nil {
+//		return
+//	}
+//	if statusCode != fasthttp.StatusOK {
+//		err = errors.New(fmt.Sprintf("request failed statusCode[%d]", statusCode))
+//		return
+//	}
+//	if body == nil {
+//		err = errors.New("response body is nil")
+//		return
+//	}
+//	m = make(map[string]interface{})
+//	if err = json.Unmarshal(body, &m); err != nil {
+//		return
+//	}
+//	return
+//}
+//
+//func Post(host string, header map[string]string, payload []byte) (body []byte, err error) {
+//	req := &fasthttp.Request{}
+//	req.SetRequestURI(host)
+//
+//	req.Header.SetContentType("application/json")
+//	req.Header.SetMethod("POST")
+//
+//	if header != nil {
+//		for k, v := range header {
+//			req.Header.Set(k, v)
+//		}
+//	}
+//
+//	req.SetBody(payload)
+//
+//	resp := &fasthttp.Response{}
+//	client := &fasthttp.Client{}
+//	if err = client.DoTimeout(req, resp, timeOut); err != nil {
+//		return
+//	}
+//	body = resp.Body()
+//	return
+//}
+//
+//// 生成32位MD5
+//func MD5(text string) string {
+//	ctx := md5.New()
+//	ctx.Write([]byte(text))
+//	return hex.EncodeToString(ctx.Sum(nil))
+//}
