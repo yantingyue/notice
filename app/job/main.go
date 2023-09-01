d25502cf45fe4f87c5dab66f4f4fb100132630ec
yantingyue <784293715@qq.com>
2023-09-01 16:36:23 +0800

    nd

--- app/job/main.go
+++ app/job/main.go
@@ -19,11 +19,11 @@ func cronjob() {
 
 	c := cron.New()
 
-	//if err := c.AddFunc("*/5 * * * * *", func() {
-	//	feishu_notice.MotorNotice("富豪", feishu_notice.UserId2)
-	//}); err != nil {
-	//	panic(err)
-	//}
+	if err := c.AddFunc("*/5 * * * * *", func() {
+		feishu_notice.MotorNotice("富豪", feishu_notice.UserId2)
+	}); err != nil {
+		panic(err)
+	}
 
 	if err := c.AddFunc("*/5 * * * * *", func() {
 		feishu_notice.MotorNotice("145", feishu_notice.UserId3)
