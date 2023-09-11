7f2479d0548e434f2b13e53ce63db2087a469c1d
yantingyue <784293715@qq.com>
2023-09-11 16:26:12 +0800

    hd

--- app/act/main.go
+++ app/act/main.go
@@ -46,10 +46,10 @@ type ResponseData struct {
 
 const (
 	b               = 1                                  //1是分解 2是置换
-	actId           = 583                                //活动id
+	actId           = 589                                //活动id
 	thread          = 2                                  //并发数
-	tokenCommon     = "f594825823eb4d01810877bac4f40d74" //勿删
-	tokenYanTingYue = "f594825823eb4d01810877bac4f40d74" //颜庭跃
+	tokenCommon     = "d038919ad0f947298bd20b382bd9bfab" //勿删
+	tokenYanTingYue = "d038919ad0f947298bd20b382bd9bfab" //颜庭跃
 	//tokenCommon       = "06f4158745ba4030a5a154191e8ae1aa" //勿删
 	//tokenYanTingYue   = "06f4158745ba4030a5a154191e8ae1aa" //颜庭跃
 	tokenYanTingYueDa = "24715fa709414f6eb364ffb6f8c13485" //颜庭跃
