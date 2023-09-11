cccfcd00ebc886a34db33b97753a68ea651ca719
zhoumuhao <1304718335@qq.com>
2023-09-11 10:44:41 +0800

    feat

--- app/job/main.go
+++ app/job/main.go
@@ -66,11 +66,11 @@ func cronjob() {
 	//	panic(err)
 	//}
 
-	//if err := c.AddFunc("*/7 * * * * *", func() {
-	//	feishu_notice.MotorNotice("蜥蜴绝缘体", feishu_notice.UserId10)
-	//}); err != nil {
-	//	panic(err)
-	//}
+	if err := c.AddFunc("*/7 * * * * *", func() {
+		feishu_notice.MotorNotice("蜥蜴绝缘体", feishu_notice.UserId10)
+	}); err != nil {
+		panic(err)
+	}
 	if err := c.AddFunc("*/7 * * * * *", func() {
 		feishu_notice.MotorNotice("法典人", feishu_notice.UserId11)
 	}); err != nil {
