a36942c3559f68a8f2e51dba30f2c5054670b317
yantingyue <784293715@qq.com>
2023-09-24 11:33:15 +0800

    d

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -401,7 +401,7 @@ var (
 
 const (
 	TimeSpace   = 20                                 //间隔时间
-	BuyNum      = 4                                  //购买数量
+	BuyNum      = 1                                  //购买数量
 	ProductName = "月母之国"                             //商品名称
 	BuyToken    = "40772eb518eb443c8c0dfefc6e604c41" //购买token
 	//BuyToken = "7b1a916684f749d19328666330f35f9d" //购买token
