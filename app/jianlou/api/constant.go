e717476581426dded206cbc08a58f6c82d0737d4
yantingyue <784293715@qq.com>
2023-09-29 16:08:52 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -167,7 +167,7 @@ var (
 )
 
 const (
-	TimeSpace   = 80      //间隔时间
+	TimeSpace   = 100     //间隔时间
 	BuyNum      = 2       //购买数量
 	ProductName = "暗黑武装龙" //商品名称
 	//BuyToken    = "65d7760dfcb14c449ec27d85ba93526d" //购买token
