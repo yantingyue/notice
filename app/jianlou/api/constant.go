1ec516609ede19bf277e1328868a51ee2f7690e9
yantingyue <784293715@qq.com>
2023-10-29 10:02:46 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -414,7 +414,7 @@ var (
 )
 
 const (
-	TimeSpace   = 25                                 //间隔时间
+	TimeSpace   = 35                                 //间隔时间
 	BuyNum      = 2                                  //购买数量
 	ProductName = "神威天将马超"                           //商品名称
 	BuyToken    = "bd0d1fc9c2ba42aa85a738b3ce600a97" //购买token
