9d5492095b4b8d3ff8983de047086907871501ed
yantingyue <784293715@qq.com>
2023-10-30 19:09:39 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -408,7 +408,7 @@ var (
 const (
 	TimeSpace   = 15                                 //间隔时间
 	BuyNum      = 2                                  //购买数量
-	ProductName = "神威天将马超"                           //商品名称
+	ProductName = "魔云腾"                              //商品名称
 	BuyToken    = "bd0d1fc9c2ba42aa85a738b3ce600a97" //购买token
 	//BuyToken = "f457f3597a04467bafe6172832ebe84d" //购买token
 	//BuyToken = "0db20208ef454d0aae633319b4863dae" //购买token ytf
