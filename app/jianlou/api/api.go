087746363424103470e69089bef9ff354065f0a9
yantingyue <784293715@qq.com>
2023-11-05 14:10:00 +0800

    hd

--- app/jianlou/api/api.go
+++ app/jianlou/api/api.go
@@ -131,15 +131,15 @@ func Grab(ctx context.Context, token string, body map[string]interface{}) {
 					go func() {
 						CreateOrderKft(ctx, BuyToken, sellInfo.SecondId, Pwd)
 					}()
-					go func() {
-						CreateOrderKft(ctx, "f457f3597a04467bafe6172832ebe84d", sellInfo.SecondId, Pwd1)
-					}()
+					//go func() {
+					//	CreateOrderKft(ctx, "f457f3597a04467bafe6172832ebe84d", sellInfo.SecondId, Pwd1)
+					//}()
 					//go func() {
 					//	CreateOrderKft(ctx, "c1555a1c07e0430a8a0137474d023025", sellInfo.SecondId, Pwd1)
 					//}()
-					//go func() {
-					//	CreateOrderKft(ctx, "8e50edd3447249658d294b910819ea92", sellInfo.SecondId, Pwd1)
-					//}()
+					go func() {
+						CreateOrderKft(ctx, "8e50edd3447249658d294b910819ea92", sellInfo.SecondId, Pwd1)
+					}()
 				}
 			}
 		}
