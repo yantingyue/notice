f73364eff58b82559cefb91ba2f6c420291351ce
yantingyue <784293715@qq.com>
2023-11-05 14:04:57 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -516,8 +516,8 @@ var (
 
 const (
 	TimeSpace   = 6                                  //间隔时间
-	BuyNum      = 2                                  //购买数量
-	ProductName = "醉道士"                              //商品名称
+	BuyNum      = 20                                 //购买数量
+	ProductName = "汉中太守"                             //商品名称
 	BuyToken    = "f8e6351734794a5395260118a7f9c5da" //购买token
 	//BuyToken = "f457f3597a04467bafe6172832ebe84d" //购买token
 	//BuyToken = "8b9d74e0145b48498b5bc704a9b045c8" //购买token ytf
