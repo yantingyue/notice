ba4044fc3003a9d3b6e6bc5207ed2b90622ffe4b
yantingyue <784293715@qq.com>
2023-11-05 11:33:57 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -515,7 +515,7 @@ var (
 )
 
 const (
-	TimeSpace   = 7                                  //间隔时间
+	TimeSpace   = 6                                  //间隔时间
 	BuyNum      = 3                                  //购买数量
 	ProductName = "司幽之国"                             //商品名称
 	BuyToken    = "f8e6351734794a5395260118a7f9c5da" //购买token
