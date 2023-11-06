0b7c535fa92f0a0018be1681321ddaa9cfcaa511
yantingyue <784293715@qq.com>
2023-11-06 19:41:41 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -515,7 +515,7 @@ var (
 )
 
 const (
-	TimeSpace   = 10                                 //间隔时间
+	TimeSpace   = 8                                  //间隔时间
 	BuyNum      = 30                                 //购买数量
 	ProductName = "乘风归"                              //商品名称
 	BuyToken    = "f8e6351734794a5395260118a7f9c5da" //购买token
