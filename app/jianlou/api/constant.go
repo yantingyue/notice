db2975969a2c39c204b3a1670bfc70ce4a1339d3
yantingyue <784293715@qq.com>
2023-11-07 23:41:05 +0800

    hd

--- app/jianlou/api/constant.go
+++ app/jianlou/api/constant.go
@@ -708,7 +708,7 @@ var (
 const (
 	TimeSpace   = 6                                  //间隔时间
 	BuyNum      = 30                                 //购买数量
-	ProductName = "扬舤起舤"                             //商品名称
+	ProductName = "扬舤起航"                             //商品名称
 	BuyToken    = "f8e6351734794a5395260118a7f9c5da" //购买token
 	//BuyToken = "f457f3597a04467bafe6172832ebe84d" //购买token
 	//BuyToken = "8b9d74e0145b48498b5bc704a9b045c8" //购买token ytf
